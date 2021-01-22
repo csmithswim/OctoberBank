cca265ff294dfba9c7a10b2abc8f7a0945afaadb
Cullen S <cullensmi@gmail.com>
2021-01-22 17:14:45 -0500

    UI interface

---
+++ src/BankTools/UI.java
@@ -0,0 +1,7 @@
+package BankTools;
+
+public interface UI {
+    //Hacker challenge add min max options to the get number within a range
+    public static int getNumber(String query);
+    String getString(String query);
+}
